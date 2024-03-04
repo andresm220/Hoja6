06c40283993ba537066286164e146400d27e2900
andresm220 <123190520+andresm220@users.noreply.github.com>
2024-03-03 22:14:06 -0600

    Opcion 1 y 2 de la implementacion HashMap realizadas

--- Cardmain.java
+++ Cardmain.java
@@ -16,142 +16,169 @@ public class Cardmain {
         boolean seguir =true;
 
         //Leemos el txt
+        while(seguir){
+            try (BufferedReader br = new BufferedReader(new FileReader("cards_desc.txt"))) {
+                String linea;
+                while ((linea = br.readLine()) != null) {
+                    // Dividir la línea en dos partes usando el carácter '|'
+                    String[] datos = linea.split("\\|");
+                    if (datos.length == 2) { // Verificar que la línea tiene exactamente dos elementos
+                        String nombreCarta = datos[0].trim();  // Eliminar espacios en blanco alrededor del nombre de la carta
+                        String tipoCarta = datos[1].trim();    // Eliminar espacios en blanco alrededor del tipo de carta
+    
+                        // Agregar la carta al HashMap
+                        cartas.put(nombreCarta, tipoCarta);
+                    } else {
+                        System.out.println("Error: línea no válida - " + linea);
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+    
+        //Menu para elegir la estructura 
+        menu();
+     
+        int estructura = scanner.nextInt();
+        scanner.nextLine();
+        switch(estructura){
 
-        try (BufferedReader reader = new BufferedReader(new FileReader("cards_desc.txt"))) {
-            String linea;
-            while ((linea = reader.readLine()) != null) {
-                // Dividir la línea en dos partes usando el carácter '|'
-                String[] datos = linea.split("\\|");
-                
-                    String nombreCarta = datos[0];  
-                    String tipoCarta = datos[1];
- 
-        
-                    // Agregar la carta 
-                    cartas.put(nombreCarta, tipoCarta);
-                    treeMap.put(nombreCarta, tipoCarta);
-                    linkedHashMap.put(nombreCarta, tipoCarta);
+            case  1 :
+            //Menu para elegir opciones del programa 
+            menu2();
+            int choice= scanner.nextInt();
+            scanner.nextLine();
+            switch (choice) {
+                //Implementacion HashMap
+                case 1:
+                //Agregar a la coleccion
+                System.out.println("Ingrese el nombre de la carta que quiere agregar a su colección");
+                String carta = scanner.nextLine();
+    
+              
+                if(cartas.containsKey(carta.trim())){
+                    Card card =new Card(carta,cartas.get(carta));
+                    coleccion.add(card);
+                    System.out.println("Carta agregada a tu coleccion ");
+                }else{
+                    System.out.println("El nombre de la carta ingresada no existe");
+                }
                
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        
-    //Menu para elegir la estructura 
-    menu();
+                break;
+                    
+    
+                case 2:
+                //Buscar tipo de una carta 
+                System.out.println("Ingrese el nombre de la carta");
+                String tipo = scanner.nextLine();
 
+                if(cartas.containsKey(tipo)){
+                    System.out.println("La carta " + tipo + " es de tipo  " + cartas.get(tipo.trim()));
 
- 
-    int estructura = scanner.nextInt();
-    switch(estructura){
-        case  1 :
-        //Menu para elegir opciones del programa 
-        menu2();
-        int choice= scanner.nextInt();
-        switch (choice) {
-            case 1:
-            System.out.println("Ingrese el nombre de la carta que quiere agregar a su colección");
-            String carta = scanner.next();
-            if(cartas.containsKey(carta)){
-                Card card =new Card(carta,cartas.get(carta));
-                coleccion.add(card);
-            }else{
-                System.out.println("El nombre de la carta ingresada no existe");
-            }
-           
-            break;
-                
+                    
+                }else{
+                    System.out.println("El nombre de la carta ingresada no existe");
+                }
+               
 
-            case 2:
-
-            break;
-
-            case 3:
-
-            break;
-            case 4:
-
-            break;
-            case 5:
-
-            break;
-            case 6:
-
-            break;
-        
-            default:
-            System.out.println("Error, ingrese una opcion valida");
-
-            break;
-        }
-        break;
-        case 2:
-        menu2();
-        int choice2= scanner.nextInt();
-        switch (choice2) {
-            case 1:
-                
-            break;
-            case 2:
-
-            break;
-
-            case 3:
-
-            break;
-            case 4:
-
-            break;
-            case 5:
-
-            break;
-            case 6:
-
-            break;
-        
-            default:
-            System.out.println("Error, ingrese una opcion valida");
+    
+                break;
+    
+                case 3:
+    
+                break;
+                case 4:
+    
+                break;
+                case 5:
+    
+                break;
+                case 6:
+    
+                break;
             
-            break;
-        }
-
-        break;
-        case 3:
-        menu2();
-        int choice3=scanner.nextInt();
-        switch (choice3) {
-            case 1:
-           
-                
+                default:
+                System.out.println("Error, ingrese una opcion valida");
+    
+                break;
+            }
             break;
             case 2:
-
+            menu2();
+            int choice2= scanner.nextInt();
+            switch (choice2) {
+                case 1:
+                    
+                break;
+                case 2:
+    
+                break;
+    
+                case 3:
+    
+                break;
+                case 4:
+    
+                break;
+                case 5:
+    
+                break;
+                case 6:
+    
+                break;
+            
+                default:
+                System.out.println("Error, ingrese una opcion valida");
+                
+                break;
+            }
+    
             break;
-
             case 3:
-
+            menu2();
+            int choice3=scanner.nextInt();
+            switch (choice3) {
+                case 1:
+               
+                    
+                break;
+                case 2:
+    
+                break;
+    
+                case 3:
+    
+                break;
+                case 4:
+    
+                break;
+                case 5:
+    
+                break;
+                case 6:
+    
+                break;
+            
+                default:
+                System.out.println("Error, ingrese una opcion valida");
+                
+                break;
+            }
             break;
-            case 4:
-
-            break;
-            case 5:
-
-            break;
-            case 6:
-
-            break;
-        
             default:
             System.out.println("Error, ingrese una opcion valida");
-            
             break;
         }
-        break;
-        default:
-        System.out.println("Error, ingrese una opcion valida");
-        break;
-    }
+    
 
+
+
+
+
+
+        }
+
+        
 
 
 
